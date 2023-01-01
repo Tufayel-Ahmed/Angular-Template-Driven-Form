e8ef67f736baa76bdc23d3ee9d8aef6c99e076ed
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2023-01-01 21:24:13 +0600

    Complete Commit

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  
+  customer = new Customer();
+
+  ngOnInit(): void {
+
+  }
+
+  save(customerForm: NgForm): void {
+    console.log(customerForm.form);
+    console.log('Saved: '+ JSON.stringify(customerForm.value));
+  }
+
+}
